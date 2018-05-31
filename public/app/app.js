4267c75ff09a5c592b98ec89ca1927fcf2ef72ca
Eliseu Codinhoto <zeucxb@gmail.com>
2018-05-31 15:08:00 -0300

    Aula 1: Código da primeira aula

---
+++ public/app/app.js
@@ -0,0 +1,9 @@
+import { handleStatus } from './utils/promise-helpers.js';
+
+document
+  .querySelector('#myButton')
+  .onclick = () =>
+    fetch('http://localhost:3000/notas')
+      .then(handleStatus)
+      .then(console.log)
+      .catch(console.log);
