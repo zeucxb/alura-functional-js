2fcdf1c9ddd1802cb40618ca5ac01bd972be39e6
Eliseu Codinhoto <zeucxb@gmail.com>
2018-06-03 04:42:07 -0300

    Aula 4: Código da quarta aula

--- public/app/app.js
+++ public/app/app.js
@@ -1,10 +1,19 @@
 import './utils/arrays-helpers.js';
 import { notasService as service } from './nota/service.js';
+import { takeUntil, debounceTime, compose, partialize } from './utils/operators.js';
+
+const operations = compose(
+  partialize(debounceTime, 500),
+  partialize(takeUntil, 3),
+);
+
+const action = operations(() =>
+  service
+    .sumItems('2143')
+    .then(console.log)
+    .catch(console.log)
+)
 
 document
   .querySelector('#myButton')
-  .onclick = () =>
-    service
-      .sumItems('2143')
-      .then(console.log)
-      .catch(console.log);
+  .onclick = action; 
