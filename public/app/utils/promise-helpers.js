07ae1e72201af8ea4c8269bd08cc08f8f9dcfdca
Eliseu Codinhoto <zeucxb@gmail.com>
2018-06-03 16:25:55 -0300

    Aula 5: Código da quinta aula

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -4,4 +4,31 @@ export const handleStatus = res =>
 export const log = param => {
   console.log(param);
   return param;
-}
+};
+
+export const timeoutPromise = (miliseconds, promise) => {
+  const timeout = new Promise((resolve, reject) =>
+    setTimeout(() =>
+      reject(`Limite da operação excedido (limite: ${miliseconds} ms)`),
+      miliseconds));
+
+  return Promise.race([
+    timeout,
+    promise
+  ]);
+};
+
+export const delay = miliseconds => data =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => resolve(data), miliseconds)
+  );
+
+export const retry = (retries, miliseconds, fn) =>
+  fn().catch(err => {
+    console.log(retries);
+    return delay(miliseconds)().then(() =>
+      retries > 1
+        ? retry(retries - 1, miliseconds, fn)
+        : Promise.reject(err)
+    );
+  });
